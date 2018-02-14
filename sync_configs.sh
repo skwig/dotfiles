84bc9768e4766684d7bef2c6cb1c0c29e47e8323
matej <matobrecka@gmail.com>
2018-02-14 17:50:18 +0100

    ranger

--- sync_configs.sh
+++ sync_configs.sh
@@ -1,3 +1,11 @@
 cp -rf ~/.config/i3 .
 cp -rf ~/.config/polybar .
+
+mkdir -p ranger
+cp -rf ~/.config/ranger/rc.conf ./ranger
+cp -rf ~/.config/ranger/rifle.conf ./ranger
+cp -rf ~/.config/ranger/scope.sh ./ranger
+cp -rf ~/.config/ranger/colorschemes ./ranger
+
 cp -rf ~/.Xresources .
+
