eea0cf445c1a900775a7145f9c5276338fd1ddc4
matej <matobrecka@gmail.com>
2024-11-03 11:57:38 +0100

    symlinks, moved ideavim

--- make-symlinks.ps1
+++ make-symlinks.ps1
@@ -1,2 +1,5 @@
 ﻿echo $PSScriptRoot
-cmd /c mklink "%LocalAppData%\Microsoft\PowerToys\Keyboard Manager\default.json" "$PSScriptRoot\PowerToys\Keyboard Manager\default.json"
+cmd /c mklink "%UserProfile%\AppData\Local\Microsoft\PowerToys\Keyboard Manager\default.json" "$PSScriptRoot\PowerToys\Keyboard Manager\default.json"
+cmd /c mklink "%UserProfile%\AppData\Local\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\settings.json" "$PSScriptRoot\wt\settings.json"
+cmd /c mklink "%UserProfile%\.glzr\glazewm\config.yaml" "$PSScriptRoot\glazewm\config.yaml"
+cmd /c mklink "%UserProfile%\.ideavimrc" "$PSScriptRoot\ideavim\.ideavimrc"
