06d957f3283f7edb4b8d8c55a1cb0754fb9f571a
matej <matobrecka@gmail.com>
2024-12-08 21:45:09 +0100

    lsp cleanup

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -1,47 +1,108 @@
 return {
   {
-    "folke/lazydev.nvim",
-    ft = "lua",
-    opts = {
-      library = {
-        { path = "luvit-meta/library", words = { "vim%.uv" } },
-      },
-    },
-  },
-  { "Bilal2453/luvit-meta", lazy = true },
-  {
     "neovim/nvim-lspconfig",
     dependencies = {
       { "williamboman/mason.nvim", config = true },
-      "williamboman/mason-lspconfig.nvim",
-      "WhoIsSethDaniel/mason-tool-installer.nvim",
+      { "williamboman/mason-lspconfig.nvim" },
+      { "WhoIsSethDaniel/mason-tool-installer.nvim" },
       { "j-hui/fidget.nvim", opts = {} },
-      "hrsh7th/cmp-nvim-lsp",
+      { "hrsh7th/cmp-nvim-lsp" },
     },
     config = function()
+      local lspconfig = require("lspconfig")
+      local telescope_builtin = require("telescope.builtin")
+      local mason = require("mason")
+      local mason_tool_installer = require("mason-tool-installer")
+      local mason_lspconfig = require("mason-lspconfig")
+      local cmp_nvim_lsp = require("cmp_nvim_lsp")
+
+      local lsps = { "gopls", "lua_ls", "stylua", "omnisharp" }
+      local capabilities =
+        vim.tbl_deep_extend("force", lspconfig.util.default_config.capabilities, cmp_nvim_lsp.default_capabilities())
+
+      lspconfig.util.default_config.capabilities = capabilities
+
+      mason.setup()
+      mason_tool_installer.setup({ ensure_installed = lsps })
+      mason_lspconfig.setup({
+        handlers = {
+          function(server_name)
+            lspconfig[server_name].setup({})
+          end,
+
+          lua_ls = function()
+            lspconfig.lua_ls.setup({
+              settings = {
+                Lua = {
+                  completion = {
+                    callSnippet = "Replace",
+                  },
+                },
+              },
+            })
+          end,
+
+          stylua = function()
+            lspconfig.stylua.setup({})
+          end,
+
+          gopls = function()
+            lspconfig.gopls.setup({
+              hints = {
+                assignVariableTypes = true,
+                compositeLiteralFields = true,
+                compositeLiteralTypes = true,
+                constantValues = true,
+                functionTypeParameters = true,
+                parameterNames = true,
+                rangeVariableTypes = true,
+              },
+              analyses = {
+                unusedparams = true,
+              },
+              staticcheck = true,
+            })
+          end,
+
+          omnisharp = function()
+            lspconfig.omnisharp.setup({})
+          end,
+        },
+      })
+
+      if vim.g.have_nerd_font then
+        local signs = { Error = "", Warn = "", Hint = "", Info = "" }
+        for type, icon in pairs(signs) do
+          local hl = "DiagnosticSign" .. type
+          vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
+        end
+      end
+
       vim.api.nvim_create_autocmd("LspAttach", {
         group = vim.api.nvim_create_augroup("lsp-attach", { clear = true }),
         callback = function(event)
+          local client = vim.lsp.get_client_by_id(event.data.client_id)
+
           local map = function(keys, func, desc, mode)
             mode = mode or "n"
             vim.keymap.set(mode, keys, func, { buffer = event.buf, desc = "LSP: " .. desc })
           end
 
           map("<leader>d", vim.lsp.buf.definition, "(jump to) [D]efinition")
-          map("<leader>u", require("telescope.builtin").lsp_references, "(jump to) [U]sages")
+          map("<leader>u", telescope_builtin.lsp_references, "(jump to) [U]sages")
           map("<leader>n", vim.lsp.buf.rename, "Re[n]ame")
           map("<leader>k", vim.lsp.buf.code_action, "Show A[k]tions")
           map("<leader>v", vim.lsp.buf.hover, "Ho[V]er")
 
-          map("<leader>ji", require("telescope.builtin").lsp_implementations, "[J]ump to [I]mplementation")
-          map("<leader>jm", require("telescope.builtin").lsp_document_symbols, "[J]ump to [M]embers")
+          map("<leader>ji", telescope_builtin.lsp_implementations, "[J]ump to [I]mplementation")
+          map("<leader>jm", telescope_builtin.lsp_document_symbols, "[J]ump to [M]embers")
           map("<leader>jr", vim.lsp.buf.signature_help, "[J]ump to Pa[R]ameters")
 
-          -- vim.keymap.set('n', 'go', '<cmd>lua vim.lsp.buf.type_definition()<cr>', opts)
-          -- vim.keymap.set({ "n", "x" }, "<F3>", "<cmd>lua vim.lsp.buf.format({async = true})<cr>", opts)
+          if client and client.supports_method(vim.lsp.protocol.Methods.textDocument_inlayHint) then
+            map("<leader>th", vim.lsp.inlay_hint.enable, "[T]oggle Inlay [H]ints")
+          end
 
           -- Highlight references of the word under the cursor on hold
-          local client = vim.lsp.get_client_by_id(event.data.client_id)
           if client and client.supports_method(vim.lsp.protocol.Methods.textDocument_documentHighlight) then
             local highlight_augroup = vim.api.nvim_create_augroup("lsp-highlight", { clear = false })
             vim.api.nvim_create_autocmd({ "CursorHold", "CursorHoldI" }, {
@@ -64,77 +125,18 @@ return {
               end,
             })
           end
-
-          -- The following code creates a keymap to toggle inlay hints in your
-          -- code, if the language server you are using supports them
-          --
-          -- This may be unwanted, since they displace some of your code
-          if client and client.supports_method(vim.lsp.protocol.Methods.textDocument_inlayHint) then
-            map("<leader>th", function()
-              vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled({ bufnr = event.buf }))
-            end, "[T]oggle Inlay [H]ints")
-          end
         end,
-      })
-
-      -- Change diagnostic symbols in the sign column (gutter)
-      if vim.g.have_nerd_font then
-        local signs = { Error = "", Warn = "", Hint = "", Info = "" }
-        for type, icon in pairs(signs) do
-          local hl = "DiagnosticSign" .. type
-          vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
-        end
-      end
-
-      local capabilities = vim.lsp.protocol.make_client_capabilities()
-      capabilities = vim.tbl_deep_extend("force", capabilities, require("cmp_nvim_lsp").default_capabilities())
-
-      local servers = {
-        gopls = {
-          hints = {
-            assignVariableTypes = true,
-            compositeLiteralFields = true,
-            compositeLiteralTypes = true,
-            constantValues = true,
-            functionTypeParameters = true,
-            parameterNames = true,
-            rangeVariableTypes = true,
-          },
-          analyses = {
-            unusedparams = true,
-          },
-          staticcheck = true,
-        },
-        lua_ls = {
-          settings = {
-            Lua = {
-              completion = {
-                callSnippet = "Replace",
-              },
-            },
-          },
-        },
-        -- csharp_ls = {},
-        omnisharp = {},
-      }
-
-      require("mason").setup()
-
-      local ensure_installed = vim.tbl_keys(servers or {})
-      vim.list_extend(ensure_installed, {
-        "stylua", -- Used to format Lua code
-      })
-      require("mason-tool-installer").setup({ ensure_installed = ensure_installed })
-
-      require("mason-lspconfig").setup({
-        handlers = {
-          function(server_name)
-            local server = servers[server_name] or {}
-            server.capabilities = vim.tbl_deep_extend("force", {}, capabilities, server.capabilities or {})
-            require("lspconfig")[server_name].setup(server)
-          end,
-        },
       })
     end,
   },
+  {
+    "folke/lazydev.nvim",
+    ft = "lua",
+    opts = {
+      library = {
+        { path = "luvit-meta/library", words = { "vim%.uv" } },
+      },
+    },
+  },
+  { "Bilal2453/luvit-meta", lazy = true },
 }
