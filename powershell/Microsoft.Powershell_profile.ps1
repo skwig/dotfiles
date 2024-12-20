87a6a10375eddb7b7258dad2551a69b61defc2b3
matej <matobrecka@gmail.com>
2024-12-20 22:53:44 +0100

    fzf integrations

--- powershell/Microsoft.Powershell_profile.ps1
+++ powershell/Microsoft.Powershell_profile.ps1
@@ -1,2 +1,12 @@
 # fzf Ctrl+T and Ctrl+R
 Set-PsFzfOption -PSReadlineChordProvider 'Ctrl+t' -PSReadlineChordReverseHistory 'Ctrl+r'
+
+function Rider-Fzf { rider $(fzf) }
+
+Set-Alias rfzf Rider-Fzf
+Set-Alias rf Rider-Fzf
+
+function Nvim-Fzf { nvim $(fzf) }
+
+Set-Alias nfzf Nvim-Fzf
+Set-Alias nf Nvim-Fzf
