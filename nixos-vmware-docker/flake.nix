56c12e550a6ae15dccdd48c4e1680901a72d81c8
matej <maotbrecka@gmail.com>
2024-12-28 12:48:41 +0100

    nixos docker hostname

--- nixos-vmware-docker/flake.nix
+++ nixos-vmware-docker/flake.nix
@@ -4,7 +4,7 @@
   };
 
   outputs = { self, nixpkgs, ... }@attrs: {
-    nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
+    nixosConfigurations.nixos-vmware-docker = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
       specialArgs = attrs;
       modules = [
