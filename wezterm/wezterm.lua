b416f50545fa0f864e56cc0c6ca378f17a371ce4
matej <maotbrecka@gmail.com>
2025-02-01 22:43:37 +0100

    wezterm

---
+++ wezterm/wezterm.lua
@@ -0,0 +1,32 @@
+local wezterm = require("wezterm")
+local act = wezterm.action
+local config = wezterm.config_builder()
+
+function OS()
+	return package.config:sub(1, 1) == "\\" and "win" or "unix"
+end
+
+config.font = wezterm.font("JetBrainsMono Nerd Font")
+config.font_size = 11
+config.color_scheme = "Vs Code Dark+ (Gogh)"
+config.window_decorations = "RESIZE"
+
+if OS() == "win" then
+	config.default_prog = { "pwsh" }
+end
+
+config.keys = {
+	{ key = "1", mods = "ALT", action = act.ActivateTab(0) },
+	{ key = "2", mods = "ALT", action = act.ActivateTab(1) },
+	{ key = "3", mods = "ALT", action = act.ActivateTab(2) },
+	{ key = "4", mods = "ALT", action = act.ActivateTab(3) },
+	{ key = "5", mods = "ALT", action = act.ActivateTab(4) },
+	{ key = "6", mods = "ALT", action = act.ActivateTab(5) },
+	{ key = "7", mods = "ALT", action = act.ActivateTab(6) },
+	{ key = "8", mods = "ALT", action = act.ActivateTab(7) },
+	{ key = "9", mods = "ALT", action = act.ActivateTab(8) },
+
+	{ key = "F", mods = "CTRL|SHIFT", action = wezterm.action.Search({ CaseInSensitiveString = "" }) },
+}
+
+return config
