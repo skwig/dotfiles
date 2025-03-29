7fc2fda0b8e8d953f3bc34f75920fedeb93b09bc
skwig <skwig@users.noreply.github.com>
2025-03-29 13:29:27 +0100

    multimonitor, nvidia driver

--- nixos/hosts/blackbox/system.nix
+++ nixos/hosts/blackbox/system.nix
@@ -7,6 +7,9 @@
 {
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
+  boot.loader.efi.efiSysMountPoint = "/boot";
+
+  boot.supportedFilesystems = [ "ntfs" ];
 
   networking.hostName = "blackbox";
   networking.networkmanager.enable = true;
@@ -23,7 +26,36 @@
 
   services.xserver.videoDrivers = [ "nvidia" ];
 
+  programs.virt-manager.enable = true;
+
+  users.groups.libvirtd.members = ["skwig"];
+
+  boot.extraModprobeConfig = "options kvm_amd nested=1";
+  virtualisation.libvirtd = {
+    enable = true;
+    qemu = {
+      package = pkgs.qemu_kvm;
+      runAsRoot = true;
+      swtpm.enable = true;
+      ovmf = {
+        enable = true;
+        packages = [(pkgs.OVMF.override {
+          secureBoot = true;
+          tpmSupport = true;
+        }).fd];
+      };
+    };
+
+    # spiceUSBRedirection.enable = true;
+  };
+
+  programs.steam.enable = true;
+
   environment.systemPackages = with pkgs; [
+    nvtop
+
+    vscodium
+
     chromium
     firefox
     brave
