4bd9b4f13e8672451656e394c3e3c83548133571
matej <matobrecka@gmail.com>
2025-04-15 22:42:21 +0200

    freelens, lsp, wezterm

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -2,10 +2,10 @@ return {
   {
     "neovim/nvim-lspconfig",
     dependencies = {
-      { "williamboman/mason.nvim", config = true },
+      { "williamboman/mason.nvim",                  config = true },
       { "williamboman/mason-lspconfig.nvim" },
       { "WhoIsSethDaniel/mason-tool-installer.nvim" },
-      { "j-hui/fidget.nvim", opts = {} },
+      { "j-hui/fidget.nvim",                        opts = {} },
       { "hrsh7th/cmp-nvim-lsp" },
       { "someone-stole-my-name/yaml-companion.nvim" },
     },
@@ -19,9 +19,9 @@ return {
       local cmp_nvim_lsp = require("cmp_nvim_lsp")
       local yaml_companion = require("yaml-companion")
 
-      local lsps = { "gopls", "lua_ls", "stylua", "omnisharp", "yamlls" }
+      local lsps = { "gopls", "lua_ls", "stylua", "omnisharp", "yamlls", "terraformls" }
       local capabilities =
-        vim.tbl_deep_extend("force", lspconfig.util.default_config.capabilities, cmp_nvim_lsp.default_capabilities())
+          vim.tbl_deep_extend("force", lspconfig.util.default_config.capabilities, cmp_nvim_lsp.default_capabilities())
 
       lspconfig.util.default_config.capabilities = capabilities
 
@@ -93,27 +93,16 @@ return {
                     validate = true,
                     hover = true,
                     completion = true,
-                    -- schemas = {
-                    --   kubernetes = "*.yaml",
-                    --   ["http://json.schemastore.org/github-workflow"] = ".github/workflows/*",
-                    --   ["http://json.schemastore.org/github-action"] = ".github/action.{yml,yaml}",
-                    --   ["http://json.schemastore.org/ansible-stable-2.9"] = "roles/tasks/*.{yml,yaml}",
-                    --   ["http://json.schemastore.org/prettierrc"] = ".prettierrc.{yml,yaml}",
-                    --   ["http://json.schemastore.org/kustomization"] = "kustomization.{yml,yaml}",
-                    --   ["http://json.schemastore.org/ansible-playbook"] = "*play*.{yml,yaml}",
-                    --   ["http://json.schemastore.org/chart"] = "Chart.{yml,yaml}",
-                    --   ["https://json.schemastore.org/dependabot-v2"] = ".github/dependabot.{yml,yaml}",
-                    --   ["https://json.schemastore.org/gitlab-ci"] = "*gitlab-ci*.{yml,yaml}",
-                    --   ["https://raw.githubusercontent.com/OAI/OpenAPI-Specification/main/schemas/v3.1/schema.json"] = "*api*.{yml,yaml}",
-                    --   ["https://raw.githubusercontent.com/compose-spec/compose-spec/master/schema/compose-spec.json"] = "*docker-compose*.{yml,yaml}",
-                    --   ["https://raw.githubusercontent.com/argoproj/argo-workflows/master/api/jsonschema/schema.json"] = "*flow*.{yml,yaml}",
-                    -- },
+                    trace = { server = "info" },
                   },
                 },
               },
             })
-
             lspconfig.yamlls.setup(cfg)
+          end,
+
+          terraformls = function()
+            lspconfig.terraformls.setup({})
           end,
         },
       })
