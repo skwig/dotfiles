0f84d0ca2b3413fe0975def13660d1b02a54aae9
mbr <user.email>
2025-04-17 00:13:27 +0200

    fix filename

--- nixos/modules/gnome.nix
+++ nixos/modules/gnome.nix
@@ -5,9 +5,20 @@
 { config, pkgs, ... }:
 
 {
-  hardware.nvidia = {
-    modesetting.enable = true;
-    open = false;
-    package = config.boot.kernelPackages.nvidiaPackages.stable;
+  services.xserver.enable = true;
+  services.xserver.displayManager.gdm.enable = true;
+  services.xserver.desktopManager.gnome.enable = true;
+
+  services.printing.enable = true;
+
+  hardware.pulseaudio.enable = false;
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+
+    alsa.enable = true;
+    alsa.support32Bit = true;
+
+    pulse.enable = true;
   };
 }
