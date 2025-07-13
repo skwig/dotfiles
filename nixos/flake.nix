9f9fdc568697a2cf261f433aa0f64d790af0988f
skwig <16136203+skwig@users.noreply.github.com>
2025-07-13 17:03:26 +0200

    nixos simplify flake

--- nixos/flake.nix
+++ nixos/flake.nix
@@ -23,77 +23,56 @@
       hyprpanel,
       ...
     }@attrs:
+    let
+      system = "x86_64-linux";
+      specialArgs = attrs // {
+        pkgs-unstable = (
+          import nixpkgs-unstable {
+            inherit system;
+            config.allowUnfree = true;
+          }
+        );
+        pkgs-24 = (
+          import nixpkgs-24 {
+            inherit system;
+            config.allowUnfree = true;
+          }
+        );
+        pkgs-pr = {
+          freelens = (import pr-freelens { inherit system; }).freelens;
+        };
+        pkgs-hyprpanel = {
+          hyprpanel = (import hyprpanel { inherit system; }).hyprpanel;
+        };
+      };
+    in
     {
-      nixosConfigurations.blackbox = nixpkgs.lib.nixosSystem rec {
-        system = "x86_64-linux";
-        specialArgs = attrs // {
-          pkgs-unstable = (
-            import nixpkgs-unstable {
-              inherit system;
-              config.allowUnfree = true;
-            }
-          );
-          pkgs-24 = (
-            import nixpkgs-24 {
-              inherit system;
-              config.allowUnfree = true;
-            }
-          );
-          pkgs-pr = {
-            freelens = (import pr-freelens { inherit system; }).freelens;
+      nixosConfigurations = {
+        blackbox = nixpkgs.lib.nixosSystem {
+          system = system;
+          specialArgs = specialArgs // {
+            username = "skwig";
+            hostname = "blackbox";
           };
-          pkgs-hyprpanel = {
-            hyprpanel = (import hyprpanel { inherit system; }).hyprpanel;
-          };
-          username = "skwig";
-          hostname = "blackbox";
+          modules = [
+            ./hosts/blackbox/configuration.nix
+            home-manager.nixosModules.default
+            { nixpkgs.overlays = [ hyprpanel.overlay ]; }
+          ];
         };
-        modules = [
-          { nixpkgs.overlays = [ hyprpanel.overlay ]; }
-          home-manager.nixosModules.default
-          ./hosts/blackbox/configuration.nix
-        ];
-      };
 
-      nixosConfigurations.blackbox2 = nixpkgs.lib.nixosSystem {
-        system = "x86_64-linux";
-        specialArgs = attrs // {
-          pkgs-pr = {
-            freelens = (import pr-freelens { system = "x86_64-linux"; }).freelens;
+        smallbox = nixpkgs.lib.nixosSystem {
+          system = system;
+          specialArgs = specialArgs // {
+            username = "skwig";
+            hostname = "smallbox";
           };
-          username = "mbr";
-          hostname = "blackbox2";
+          modules = [
+            ./hosts/smallbox/configuration.nix
+            home-manager.nixosModules.default
+            { nixpkgs.overlays = [ hyprpanel.overlay ]; }
+          ];
         };
-        modules = [
-          { nixpkgs.overlays = [ hyprpanel.overlay ]; }
-          home-manager.nixosModules.default
-          ./hosts/blackbox2/configuration.nix
-        ];
-      };
-
-      nixosConfigurations.smallbox = nixpkgs.lib.nixosSystem rec {
-        system = "x86_64-linux";
-        specialArgs = attrs // {
-          pkgs-unstable = (
-            import nixpkgs-unstable {
-              inherit system;
-              config.allowUnfree = true;
-            }
-          );
-          pkgs-pr = {
-            freelens = (import pr-freelens { inherit system; }).freelens;
-          };
-          pkgs-hyprpanel = {
-            hyprpanel = (import hyprpanel { inherit system; }).hyprpanel;
-          };
-          username = "skwig";
-          hostname = "smallbox";
-        };
-        modules = [
-          { nixpkgs.overlays = [ hyprpanel.overlay ]; }
-          home-manager.nixosModules.default
-          ./hosts/smallbox/configuration.nix
-        ];
       };
 
       nixosModules = {
