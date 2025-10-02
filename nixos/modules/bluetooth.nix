10c9f5aca3afb188d83417798a3b3674ae68c40d
skwig <16136203+skwig@users.noreply.github.com>
2025-10-02 19:06:09 +0200

    feat(bluetooth): disable mic+audio mode

--- nixos/modules/bluetooth.nix
+++ nixos/modules/bluetooth.nix
@@ -9,4 +9,15 @@
     blueberry
     blueman
   ];
+
+  # Disallows mic+audio combo on BT headset,
+  # making it not get grabbed and switching to a lower quality output
+  services.pipewire.wireplumber.extraConfig."10-bluez" = {
+    "monitor.bluez.properties" = {
+      "bluez5.roles" = [
+        "a2dp_sink"
+        "a2dp_source"
+      ];
+    };
+  };
 }
