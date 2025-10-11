95a74deac1b01b4e376f826146ea1ce3f4a25cf6
skwig <16136203+skwig@users.noreply.github.com>
2025-10-11 14:18:43 +0200

    chore(wezterm): format

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -7,7 +7,7 @@ end
 
 config.font = wezterm.font({
   family = "JetBrainsMono Nerd Font",
-  harfbuzz_features = { 'calt=0', 'clig=0', 'liga=0' }
+  harfbuzz_features = { "calt=0", "clig=0", "liga=0" },
 })
 config.font_size = 11
 config.color_scheme = "Vs Code Dark+ (Gogh)"
@@ -16,8 +16,8 @@ config.colors = {
     active_tab = {
       bg_color = "#1e1e1e",
       fg_color = "#c0c0c0",
-    }
-  }
+    },
+  },
 }
 
 config.use_fancy_tab_bar = false
