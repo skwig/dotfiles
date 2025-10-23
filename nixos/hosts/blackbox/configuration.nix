dd7cc17a6bbcf9fb4b21009c6c9d3cd8ba550f8f
skwig <16136203+skwig@users.noreply.github.com>
2025-10-24 00:28:49 +0200

    feat(blackbox): enable openssh

--- nixos/hosts/blackbox/configuration.nix
+++ nixos/hosts/blackbox/configuration.nix
@@ -85,4 +85,21 @@
     # vmware-workstation
     pkgs-pr.freelens
   ];
+
+  users.users.${username}.openssh.authorizedKeys.keys = [
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINKBYbvBqHC1HbBgrSXPVc3UDqMjCqjr/k1jqQIpnPJR skwig@blackbox"
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDfbf7RIFcpdW+9ryqeDoRYEeors8vMRj2ILh+UC66xm skwig@smallbox"
+  ];
+
+  services.openssh = {
+    enable = true;
+    ports = [ 17937 ];
+    openFirewall = true;
+    settings = {
+      PermitRootLogin = "no";
+      PasswordAuthentication = false;
+      KbdInteractiveAuthentication = false;
+      AllowUsers = [ username ];
+    };
+  };
 }
