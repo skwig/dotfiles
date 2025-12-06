83661458e687e6ec1d9d1632044fb92889626e2a
skwig <16136203+skwig@users.noreply.github.com>
2025-12-06 21:49:26 +0100

    feat(nvim): add rest.nvim run command

--- nvim/lua/plugins/rest.lua
+++ nvim/lua/plugins/rest.lua
@@ -7,4 +7,17 @@ return {
       table.insert(opts.ensure_installed, "http")
     end,
   },
+  config = function()
+    require("rest-nvim").setup({})
+
+    vim.api.nvim_create_autocmd("FileType", {
+      pattern = "http",
+      callback = function()
+        vim.keymap.set("n", "<leader>k", "<cmd>Rest run<CR>", {
+          buffer = true,
+          desc = "Run REST request",
+        })
+      end,
+    })
+  end,
 }
