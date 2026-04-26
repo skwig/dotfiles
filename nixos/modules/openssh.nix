52ce13dda65e2cfe60a486a71ea4177f5993ffe2
skwig <16136203+skwig@users.noreply.github.com>
2026-04-26 20:30:47 +0200

    feat(openssh): add android key

--- nixos/modules/openssh.nix
+++ nixos/modules/openssh.nix
@@ -7,6 +7,7 @@
   users.users.${username}.openssh.authorizedKeys.keys = [
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINKBYbvBqHC1HbBgrSXPVc3UDqMjCqjr/k1jqQIpnPJR skwig@blackbox"
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDfbf7RIFcpdW+9ryqeDoRYEeors8vMRj2ILh+UC66xm skwig@smallbox"
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPK6B/HcG55FYYlfz8Ok9Z6B1L2MnEZpZqgumq7RJJSI skwig@android"
   ];
 
   services.openssh = {
