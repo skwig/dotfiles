907de1ad98b25183c89914180fcd65cdee06de20
skwig <16136203+skwig@users.noreply.github.com>
2026-05-12 21:19:19 +0200

    feat(opencode): make supowerpowers command windows friendly

--- nixos/modules/dev-ai.nix
+++ nixos/modules/dev-ai.nix
@@ -20,11 +20,11 @@
     {
       home.file.".config/opencode/opencode.json".source =
         config.lib.file.mkOutOfStoreSymlink /${dotfiles}/opencode/opencode.json;
-      home.file.".config/opencode/command/superpowers:brainstorming.md".source =
-        config.lib.file.mkOutOfStoreSymlink "/${dotfiles}/opencode/command/superpowers:brainstorming.md";
-      home.file.".config/opencode/command/superpowers:executing-plans.md".source =
-        config.lib.file.mkOutOfStoreSymlink "/${dotfiles}/opencode/command/superpowers:executing-plans.md";
-      home.file.".config/opencode/command/superpowers:writing-plans.md".source =
-        config.lib.file.mkOutOfStoreSymlink "/${dotfiles}/opencode/command/superpowers:writing-plans.md";
+      home.file.".config/opencode/command/superpowers-brainstorming.md".source =
+        config.lib.file.mkOutOfStoreSymlink "/${dotfiles}/opencode/command/superpowers-brainstorming.md";
+      home.file.".config/opencode/command/superpowers-executing-plans.md".source =
+        config.lib.file.mkOutOfStoreSymlink "/${dotfiles}/opencode/command/superpowers-executing-plans.md";
+      home.file.".config/opencode/command/superpowers-writing-plans.md".source =
+        config.lib.file.mkOutOfStoreSymlink "/${dotfiles}/opencode/command/superpowers-writing-plans.md";
     };
 }
