c92c2bddb2d8d1931e8b6f52fe2b9edf8dd22252
skwig <16136203+skwig@users.noreply.github.com>
2026-05-12 23:16:33 +0200

    feat(win): streamline install by using import

--- win-install.ps1
+++ win-install.ps1
@@ -1,22 +1,8 @@
 # https://scoop.sh/
 # Invoke-RestMethod -Uri https://get.scoop.sh | Invoke-Expression
 
-winget install Microsoft.Powershell
-winget install wezterm
-winget install GlazeWM
-winget install DEVCOM.JetBrainsMonoNerdFont
-winget install nvim
-winget install JesseDuffield.lazygit
-winget install fastfetch
-winget install JanDeDobbeleer.OhMyPosh
+# winget export wingetfile.json --include-versions
+winget import ./wingetfile.json
 
-winget install fzf
-winget install sharkdp.fd
-winget install BurntSushi.ripgrep.MSVC
-winget install jqlang.jq
-
-scoop bucket add extras
-scoop install extras/psfzf
-scoop install make
-scoop install mingw
-}
+# scoop export > scoopfile.json
+scoop import ./scoopfile.json
