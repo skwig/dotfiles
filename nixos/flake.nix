cd30d31ba3b526ca50789f144a67a301422c45ce
skwig <16136203+skwig@users.noreply.github.com>
2026-06-12 11:12:19 +0200

    feat(hyprland): run with patched xkb instead of downgraded libxkbcommon

--- nixos/flake.nix
+++ nixos/flake.nix
@@ -3,8 +3,7 @@
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-26.05";
     nixpkgs-unstable.url = "github:nixos/nixpkgs?ref=nixos-unstable";
     nixpkgs-cuttingedge.url = "github:nixos/nixpkgs?ref=nixos-unstable";
-    # nixpkgs-hypr.url = "github:nixos/nixpkgs?ref=nixos-unstable";
-    nixpkgs-hypr.url = "github:skwig/nixpkgs?ref=nixos-unstable-libxkbcommon-1.11.0";
+    nixpkgs-hypr.url = "github:skwig/nixpkgs?ref=nixos-unstable-hyprland-xkb-patch";
 
     home-manager.url = "github:nix-community/home-manager?ref=release-26.05";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
